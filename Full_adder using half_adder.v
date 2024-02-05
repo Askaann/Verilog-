c73a377bb2503a64f8625e028b6c9f812a3d05ff
Askaann <103949981+Askaann@users.noreply.github.com>
2024-02-05 20:23:08 +0530

    Update Full_adder using half_adder.v

--- Full_adder using half_adder.v
+++ Full_adder using half_adder.v
@@ -1,6 +1,22 @@
 // verilog code Full Adder
-module Full_Adder(A,B,Cin,Sum,Cout);
-  input A,B,Cin;
-  output Sum,Cout;
+module full_adder (
+	input a,b,cin, 
+	output sum, c_out);
+  //sum0=sum of 1st half adder, cout0=carry out of 1st half adder, cout1=carry out of 2nd half adder
+	wire sum0,cout0, cout1; 
 
-  Half_adder
+	half_adder a1(a,b,sum0,cout0);
+	half_adder a2(sum0,cin,sum,cout1);
+	or a3(c_out,cout1,cout0);
+	
+	
+endmodule
+
+
+module half_adder(
+	input a,b,
+	output sum,cout);
+	assign sum = a^b;
+	assign cout = a*b;
+endmodule
+
